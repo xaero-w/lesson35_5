b3777ff34cc0bee41c7fc24b2945362a006b82f7
xaero-w <d_shalimov@mail.ru>
2020-05-14 18:58:58 +0300

    add dir lib

---
+++ lib/film.rb
@@ -0,0 +1,12 @@
+class Film
+  attr_reader :title, :director
+  def initialize(title, director)
+    @title = title
+    @director = director
+  end
+
+  def to_s
+    # Стивен Спилберг — Список Шиндлера
+    "#{@director} — #{@title}"
+  end
+end
